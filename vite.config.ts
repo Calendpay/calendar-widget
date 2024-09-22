9842a3e7e904383c55337953f7ea47eca36bf9e6
Łukasz Męderak <lukasz.mederak@gmail.com>
2024-09-22 21:08:05 +0200

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/main.tsx"), // Główny plik wejściowy React/TypeScript
+      entry: "src/main.tsx", // Główny plik wejściowy React/TypeScript
       name: "CalendpayWidget", // Nazwa eksportowanej biblioteki
       fileName: (format) => `calendpay-widget.${format}.js`,
       formats: ["umd"], // Format outputu (UMD dla kompatybilności z osadzaniem w HTML)
