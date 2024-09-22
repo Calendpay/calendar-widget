dc4be917c37d23a3560fdb745d0a3d4ced79224b
Łukasz Męderak <lukasz.mederak@gmail.com>
2024-09-22 17:18:59 +0200

    Change vite config v2

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppointmentContent from "./AppointmentContent.tsx"; // Twój główny komponent
 
-if (typeof process === "undefined") {
-  (window as any).process = {
-    env: {
-      NODE_ENV: "production",
-    },
-  };
-}
-
 // Funkcja, która osadza kalendarz w dowolnym elemencie HTML
 const renderCalendarWidget = (props: {
   containerId: string;
   productId: string;
   organizationId: string;
 }) => {
+  process.env.NODE_ENV = "production";
+
   const container = document.getElementById(props.containerId); // Znajduje element HTML po ID
   if (!container) {
     console.error(`Element with ID ${props.containerId} not found.`);
